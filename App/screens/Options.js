96f29611a8208c3aed3d5e68dd96b3917ece9971
Jessica Paul <paul.jessica95@gmail.com>
2021-05-04 14:40:07 -0400

    add links

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { SafeAreaView, ScrollView } from 'react-native';
+import { SafeAreaView, ScrollView, Linking, Alert } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 
 import colors from '../constants/colors';
 import { RowItem, RowSeparator } from '../components/Row';
+
+
+const openUrl = url => {
+    return Linking.openURL(url).catch(() => {
+        Alert.alert('Sorry, something went wrong.', 'Please try again later.');
+    });
+}
 
 export default () => {
     // ScrollView is good for a few items, not efficient for 1000s of items
@@ -22,7 +29,7 @@ export default () => {
 
             <RowItem 
                 text='React Native Basics'
-                onPress={() => alert('pressed')}
+                onPress={() => openUrl('https://reactnativebasics.com')}
                 rightIcon={<Entypo name='export' size={20} color={colors.blue} />}
             />
 
@@ -30,7 +37,7 @@ export default () => {
 
             <RowItem 
                 text='React Native by Example'
-                onPress={() => alert('pressed')}
+                onPress={() => openUrl('https://reactnativebyexample.com')}
                 rightIcon={<Entypo name='export' size={20} color={colors.blue} />}
             />
             </ScrollView>
