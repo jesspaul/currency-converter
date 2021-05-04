62775ef380b1f52ea97d63fd461f80e9b7b8cdf5
Jessica Paul <paul.jessica95@gmail.com>
2021-05-04 14:32:30 -0400

    add scroll

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 
 import colors from '../constants/colors';
 import { RowItem, RowSeparator } from '../components/Row';
 
 export default () => {
+    // ScrollView is good for a few items, not efficient for 1000s of items
+
+    // set SafeAreaView to flex: 1 to take up all available space
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{flex: 1}}>
+            <ScrollView>
             <RowItem 
                 text='Themes'
                 onPress={() => alert('pressed')}
@@ -29,6 +33,7 @@ export default () => {
                 onPress={() => alert('pressed')}
                 rightIcon={<Entypo name='export' size={20} color={colors.blue} />}
             />
+            </ScrollView>
         </SafeAreaView>
     )
 }
