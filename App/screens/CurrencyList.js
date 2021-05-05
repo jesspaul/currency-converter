3eb48bc943274d80d10f760179e841533bb921ed
Jessica Paul <paul.jessica95@gmail.com>
2021-05-04 20:16:10 -0400

    designate currently selected currency

--- App/screens/CurrencyList.js
+++ App/screens/CurrencyList.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { StatusBar, FlatList, View } from 'react-native';
+import { StatusBar, FlatList, View, StyleSheet } from 'react-native';
 import { useSafeArea } from 'react-native-safe-area-context';
+import { Entypo } from '@expo/vector-icons';
 
 import colors from '../constants/colors';
 import currencies from '../data/currencies.json';
 import { RowItem, RowSeparator } from '../components/Row';
 
-export default ({ navigation }) => {
+const styles = StyleSheet.create({
+    icon: {
+        width: 30,
+        height: 30,
+        backgroundColor: colors.blue,
+        borderRadius: 15,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
+
+export default ({ navigation, route = {} }) => {
     const insets = useSafeArea();
+    const params = route.params || {};
 
     return (
         <View style={{ backgroundColor: colors.white }}>
@@ -15,7 +28,14 @@ export default ({ navigation }) => {
             <FlatList
                 data={currencies}
                 renderItem={({ item }) => {
-                    return <RowItem text={item} onPress={() => navigation.pop()} />
+                    const selected = params.activeCurrency === item;
+                    return <RowItem text={item} onPress={() => navigation.pop()} rightIcon={
+                        selected && (
+                            <View style={styles.icon}>
+                                <Entypo name='check' size={20} color={colors.white} />
+                            </View>
+                        )
+                    } />
                 }}
                 keyExtractor={(item) => item}
                 ItemSeparatorComponent={() => <RowSeparator />}
