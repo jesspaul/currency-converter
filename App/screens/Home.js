049e1d980f99f0ac51281f3c6d65df8159dc4325
Jessica Paul <paul.jessica95@gmail.com>
2021-05-04 21:20:49 -0400

    add Context

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet, StatusBar, Image, Dimensions, Text, ScrollView, TouchableOpacity } from 'react-native';
 import { format } from 'date-fns';
 import { Entypo } from '@expo/vector-icons';
@@ -8,6 +8,7 @@ import colors from '../constants/colors';
 import { ConversionInput } from '../components/ConversionInput';
 import { Button } from '../components/Button';
 import { KeyboardSpacer } from '../components/KeyboardSpacer';
+import { ConversionContext } from '../util/ConversionContext';
 
 const screen = Dimensions.get('window');
 
@@ -52,16 +53,10 @@ const styles = StyleSheet.create({
 });
 
 export default ({ navigation }) => {
-    const [baseCurrency, setBaseCurrency] = useState('USD');
-    const [quoteCurrency, setQuoteCurrency] = useState('GBP');
     const [value, setValue] = useState('100');
     const conversionRate = 0.8345;
     const date = new Date();
-
-    const swapCurrencies = () => {
-        setBaseCurrency(quoteCurrency);
-        setQuoteCurrency(baseCurrency);
-    }
+    const { baseCurrency, quoteCurrency, swapCurrencies, setBaseCurrency, setQuoteCurrency } = useContext(ConversionContext);
 
     const [scrollEnabled, setScrollEnabled] = useState(false);
 
